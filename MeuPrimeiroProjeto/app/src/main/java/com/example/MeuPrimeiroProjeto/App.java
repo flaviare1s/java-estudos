9a89cc4fac4f3ad274cb42795661ec1b5ae128b4
flaviare1s <flaviarster@gmail.com>
2025-07-06 08:15:54 -0300

    Organiza repositório para vários projetos do curso

---
+++ MeuPrimeiroProjeto/app/src/main/java/com/example/MeuPrimeiroProjeto/App.java
@@ -0,0 +1,11 @@
+package com.example.MeuPrimeiroProjeto;
+
+public class App {
+
+    public static void main(String[] args) {
+          
+        // Comentário de linha
+        System.out.println("Hello World!");
+        System.out.println("Olá, Mundo!");
+    }
+}
