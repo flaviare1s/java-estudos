44fdc47a3e2b22e9db076a377e57da76f38dd356
flaviare1s <flaviarster@gmail.com>
2025-07-07 20:58:38 -0300

    Métodos setters e getters

--- Classes/app/src/main/java/com/example/Classes/App.java
+++ Classes/app/src/main/java/com/example/Classes/App.java
@@ -12,10 +12,10 @@ public class App {
         Scanner leitor = new Scanner(System.in);
         
         System.out.println("Digite o peso: ");
-        objetoPessoa.peso = leitor.nextFloat();
+        objetoPessoa.setPeso (leitor.nextFloat());
         
         System.out.println("Digite a altura: ");
-        objetoPessoa.altura = leitor.nextFloat();
+        objetoPessoa.setAltura(leitor.nextFloat());
         
         System.out.println("IMC: " + objetoPessoa.calcularIMC());
           
--- Classes/app/src/main/java/com/example/Classes/Pessoa.java
+++ Classes/app/src/main/java/com/example/Classes/Pessoa.java
@@ -3,13 +3,29 @@ package com.example.Classes;
 public class Pessoa {
     
     // Atributos
-    float peso;
-    float altura;
+    private float peso;
+    private float altura;
     
     // Métodos
     public float calcularIMC() {
         float imc = peso / (altura * altura);
         return imc;
+    }
+    
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
+    public float getPeso(float peso) {
+        return peso;
+    }
+    
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    public float getAltura(float altura) {
+        return altura;
     }
     
 }
