f5bef5e6449749037b45b4b082fa35a20922447f
flaviare1s <flaviarster@gmail.com>
2025-07-07 15:53:24 -0300

    Estruturas de repetição em Java

---
+++ ExercicioRepeticao/app/src/main/java/com/example/ExercicioRepeticao/App.java
@@ -0,0 +1,32 @@
+package com.example.ExercicioRepeticao;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Scanner leitorScanner = new Scanner(System.in);
+        
+       char desejaContinuar = 'S';
+       
+       while(desejaContinuar == 'S' || desejaContinuar == 's' ) {
+           
+           System.out.println("Digite um numero");
+           int numero = leitorScanner.nextInt();
+           
+           if(numero == 0) {
+               System.out.println("O numero eh zero!");
+           } else {
+               if(numero > 0) {
+                   System.out.println("O numero eh positivo!");
+               } else {
+                   System.out.println("O numero eh negativo!");
+               }
+           }
+           
+          System.out.println("Deseja continuar? S - sim / N - nao");
+          desejaContinuar = leitorScanner.next().charAt(0);
+           
+       }
+        
+    }
+}
---
+++ While/app/src/main/java/com/example/While/App.java
@@ -0,0 +1,23 @@
+package com.example.While;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int totalAlunos = 10;
+
+        Scanner leitorScanner = new Scanner(System.in);
+
+        while (totalAlunos > 0) {
+            String nomeAluno = leitorScanner.nextLine();
+            int idadeAluno = leitorScanner.nextInt();
+
+            System.out.println("O nome do aluno é: " + nomeAluno + 
+                    " e sua idade é " + idadeAluno + " anos.");
+            
+            totalAlunos--;
+        }
+    }
+}
